9f70c0d0abe1445cd5074f312cd32ca7a4c907d7
Randolf Jung <jrandolf@chromium.org>
2023-07-04 15:34:00 +0200

    chore: update webdriver schema

--- examples/webdriver-bidi/webdriver-bidi.ts
+++ examples/webdriver-bidi/webdriver-bidi.ts
@@ -10,6 +10,30 @@ export type CommandData =
   | ScriptCommand
   | SessionCommand;
 export type EmptyParams = Extensible;
+export type Message = CommandResponse | ErrorResponse | Event;
+export type CommandResponse = {
+  id: JsUint;
+  result: ResultData;
+} & Extensible;
+export type ErrorResponse = {
+  id: JsUint | null;
+  error: ErrorCode;
+  message: string;
+  stacktrace?: string;
+} & Extensible;
+export type ResultData =
+  | BrowsingContextResult
+  | EmptyResult
+  | NetworkResult
+  | ScriptResult
+  | SessionResult;
+export type EmptyResult = Extensible;
+export type Event = EventData & Extensible;
+export type EventData =
+  | BrowsingContextEvent
+  | LogEvent
+  | NetworkEvent
+  | ScriptEvent;
 export type Extensible = {
   [key: string]: any;
 };
@@ -21,12 +45,30 @@ export type JsInt = number;
  * Must be between `0` and `9007199254740991`, inclusive.
  */
 export type JsUint = number;
+export const enum ErrorCode {
+  InvalidArgument = "invalid argument",
+  InvalidSessionId = "invalid session id",
+  MoveTargetOutOfBounds = "move target out of bounds",
+  NoSuchAlert = "no such alert",
+  NoSuchElement = "no such element",
+  NoSuchFrame = "no such frame",
+  NoSuchHandle = "no such handle",
+  NoSuchNode = "no such node",
+  NoSuchScript = "no such script",
+  SessionNotCreated = "session not created",
+  UnableToCaptureScreen = "unable to capture screen",
+  UnableToCloseBrowser = "unable to close browser",
+  UnknownCommand = "unknown command",
+  UnknownError = "unknown error",
+  UnsupportedOperation = "unsupported operation",
+}
 export type SessionCommand =
   | Session.End
   | Session.New
   | Session.Status
   | Session.Subscribe
   | Session.Unsubscribe;
+export type SessionResult = Session.NewResult | Session.StatusResult;
 export namespace Session {
   export type CapabilitiesRequest = {
     alwaysMatch?: Session.CapabilityRequest;
@@ -67,6 +109,12 @@ export namespace Session {
   };
 }
 export namespace Session {
+  export type StatusResult = {
+    ready: boolean;
+    message: string;
+  };
+}
+export namespace Session {
   export type New = {
     method: "session.new";
     params: Session.NewParameters;
@@ -75,6 +123,31 @@ export namespace Session {
 export namespace Session {
   export type NewParameters = {
     capabilities: Session.CapabilitiesRequest;
+  };
+}
+export namespace Session {
+  export type NewResult = {
+    sessionId: string;
+    capabilities: {
+      acceptInsecureCerts: boolean;
+      browserName: string;
+      browserVersion: string;
+      platformName: string;
+      proxy: {
+        proxyType?: "pac" | "direct" | "autodetect" | "system" | "manual";
+        proxyAutoconfigUrl?: string;
+        ftpProxy?: string;
+        httpProxy?: string;
+        noProxy?: [...string[]];
+        sslProxy?: string;
+        socksProxy?: string
+        /**
+         * Must be between `0` and `255`, inclusive.
+         */;
+        socksVersion?: number;
+      };
+      setWindowRect: boolean;
+    } & Extensible;
   };
 }
 export namespace Session {
@@ -111,11 +184,48 @@ export type BrowsingContextCommand =
   | BrowsingContext.Navigate
   | BrowsingContext.Print
   | BrowsingContext.Reload;
+export type BrowsingContextResult =
+  | BrowsingContext.CaptureScreenshotResult
+  | BrowsingContext.CreateResult
+  | BrowsingContext.GetTreeResult
+  | BrowsingContext.NavigateResult
+  | BrowsingContext.PrintResult;
+export type BrowsingContextEvent =
+  | BrowsingContext.ContextCreated
+  | BrowsingContext.ContextDestroyed
+  | BrowsingContext.NavigationStarted
+  | BrowsingContext.FragmentNavigated
+  | BrowsingContext.DomContentLoaded
+  | BrowsingContext.Load
+  | BrowsingContext.DownloadWillBegin
+  | BrowsingContext.NavigationAborted
+  | BrowsingContext.NavigationFailed
+  | BrowsingContext.UserPromptClosed
+  | BrowsingContext.UserPromptOpened;
 export namespace BrowsingContext {
   export type BrowsingContext = string;
 }
 export namespace BrowsingContext {
+  export type InfoList = [...BrowsingContext.Info[]];
+}
+export namespace BrowsingContext {
+  export type Info = {
+    context: BrowsingContext.BrowsingContext;
+    url: string;
+    children: BrowsingContext.InfoList | null;
+    parent?: BrowsingContext.BrowsingContext | null;
+  };
+}
+export namespace BrowsingContext {
   export type Navigation = string;
+}
+export namespace BrowsingContext {
+  export type NavigationInfo = {
+    context: BrowsingContext.BrowsingContext;
+    navigation: BrowsingContext.Navigation | null;
+    timestamp: JsUint;
+    url: string;
+  };
 }
 export namespace BrowsingContext {
   export const enum ReadinessState {
@@ -133,6 +243,33 @@ export namespace BrowsingContext {
 export namespace BrowsingContext {
   export type CaptureScreenshotParameters = {
     context: BrowsingContext.BrowsingContext;
+    clip?: BrowsingContext.ClipRectangle;
+  };
+}
+export namespace BrowsingContext {
+  export type ClipRectangle =
+    | BrowsingContext.BoxClipRectangle
+    | BrowsingContext.ElementClipRectangle;
+}
+export namespace BrowsingContext {
+  export type ElementClipRectangle = {
+    type: "element";
+    element: Script.SharedReference;
+    scrollIntoView?: boolean;
+  };
+}
+export namespace BrowsingContext {
+  export type BoxClipRectangle = {
+    type: "viewport";
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+  };
+}
+export namespace BrowsingContext {
+  export type CaptureScreenshotResult = {
+    data: string;
   };
 }
 export namespace BrowsingContext {
@@ -165,6 +302,11 @@ export namespace BrowsingContext {
   };
 }
 export namespace BrowsingContext {
+  export type CreateResult = {
+    context: BrowsingContext.BrowsingContext;
+  };
+}
+export namespace BrowsingContext {
   export type GetTree = {
     method: "browsingContext.getTree";
     params: BrowsingContext.GetTreeParameters;
@@ -174,6 +316,11 @@ export namespace BrowsingContext {
   export type GetTreeParameters = {
     maxDepth?: JsUint;
     root?: BrowsingContext.BrowsingContext;
+  };
+}
+export namespace BrowsingContext {
+  export type GetTreeResult = {
+    contexts: BrowsingContext.InfoList;
   };
 }
 export namespace BrowsingContext {
@@ -200,6 +347,12 @@ export namespace BrowsingContext {
     context: BrowsingContext.BrowsingContext;
     url: string;
     wait?: BrowsingContext.ReadinessState;
+  };
+}
+export namespace BrowsingContext {
+  export type NavigateResult = {
+    navigation: BrowsingContext.Navigation | null;
+    url: string;
   };
 }
 export namespace BrowsingContext {
@@ -279,6 +432,11 @@ export namespace BrowsingContext {
   };
 }
 export namespace BrowsingContext {
+  export type PrintResult = {
+    data: string;
+  };
+}
+export namespace BrowsingContext {
   export type Reload = {
     method: "browsingContext.reload";
     params: BrowsingContext.ReloadParameters;
@@ -309,9 +467,227 @@ export namespace BrowsingContext {
     height: JsUint;
   };
 }
+export namespace BrowsingContext {
+  export type ContextCreated = {
+    method: "browsingContext.contextCreated";
+    params: BrowsingContext.Info;
+  };
+}
+export namespace BrowsingContext {
+  export type ContextDestroyed = {
+    method: "browsingContext.contextDestroyed";
+    params: BrowsingContext.Info;
+  };
+}
+export namespace BrowsingContext {
+  export type NavigationStarted = {
+    method: "browsingContext.navigationStarted";
+    params: BrowsingContext.NavigationInfo;
+  };
+}
+export namespace BrowsingContext {
+  export type FragmentNavigated = {
+    method: "browsingContext.fragmentNavigated";
+    params: BrowsingContext.NavigationInfo;
+  };
+}
+export namespace BrowsingContext {
+  export type DomContentLoaded = {
+    method: "browsingContext.domContentLoaded";
+    params: BrowsingContext.NavigationInfo;
+  };
+}
+export namespace BrowsingContext {
+  export type Load = {
+    method: "browsingContext.load";
+    params: BrowsingContext.NavigationInfo;
+  };
+}
+export namespace BrowsingContext {
+  export type DownloadWillBegin = {
+    method: "browsingContext.downloadWillBegin";
+    params: BrowsingContext.NavigationInfo;
+  };
+}
+export namespace BrowsingContext {
+  export type NavigationAborted = {
+    method: "browsingContext.navigationAborted";
+    params: BrowsingContext.NavigationInfo;
+  };
+}
+export namespace BrowsingContext {
+  export type NavigationFailed = {
+    method: "browsingContext.navigationFailed";
+    params: BrowsingContext.NavigationInfo;
+  };
+}
+export namespace BrowsingContext {
+  export type UserPromptClosed = {
+    method: "browsingContext.userPromptClosed";
+    params: BrowsingContext.UserPromptClosedParameters;
+  };
+}
+export namespace BrowsingContext {
+  export type UserPromptClosedParameters = {
+    context: BrowsingContext.BrowsingContext;
+    accepted: boolean;
+    userText?: string;
+  };
+}
+export namespace BrowsingContext {
+  export type UserPromptOpened = {
+    method: "browsingContext.userPromptOpened";
+    params: BrowsingContext.UserPromptOpenedParameters;
+  };
+}
+export namespace BrowsingContext {
+  export type UserPromptOpenedParameters = {
+    context: BrowsingContext.BrowsingContext;
+    type: "alert" | "confirm" | "prompt" | "beforeunload";
+    message: string;
+  };
+}
 export type NetworkCommand = {};
+export type NetworkResult = {};
+export type NetworkEvent =
+  | Network.BeforeRequestSent
+  | Network.FetchError
+  | Network.ResponseStarted
+  | Network.ResponseCompleted;
+export namespace Network {
+  export type BaseParameters = {
+    context: BrowsingContext.BrowsingContext | null;
+    navigation: BrowsingContext.Navigation | null;
+    redirectCount: JsUint;
+    request: Network.RequestData;
+    timestamp: JsUint;
+  };
+}
+export namespace Network {
+  export type Cookie = {
+    name: string;
+    value?: string;
+    binaryValue?: [number];
+    domain: string;
+    path: string;
+    expires?: JsUint;
+    size: JsUint;
+    httpOnly: boolean;
+    secure: boolean;
+    sameSite: "strict" | "lax" | "none";
+  };
+}
+export namespace Network {
+  export type FetchTimingInfo = {
+    timeOrigin: number;
+    requestTime: number;
+    redirectStart: number;
+    redirectEnd: number;
+    fetchStart: number;
+    dnsStart: number;
+    dnsEnd: number;
+    connectStart: number;
+    connectEnd: number;
+    tlsStart: number;
+    requestStart: number;
+    responseStart: number;
+    responseEnd: number;
+  };
+}
+export namespace Network {
+  export type Header = {
+    name: string;
+    value?: string;
+    binaryValue?: [number];
+  };
+}
+export namespace Network {
+  export type Initiator = {
+    type: "parser" | "script" | "preflight" | "other";
+    columnNumber?: JsUint;
+    lineNumber?: JsUint;
+    stackTrace?: Script.StackTrace;
+    request?: Network.Request;
+  };
+}
 export namespace Network {
   export type Request = string;
+}
+export namespace Network {
+  export type RequestData = {
+    request: Network.Request;
+    url: string;
+    method: string;
+    headers: [...Network.Header[]];
+    cookies: [...Network.Cookie[]];
+    headersSize: JsUint;
+    bodySize: JsUint | null;
+    timings: Network.FetchTimingInfo;
+  };
+}
+export namespace Network {
+  export type ResponseContent = {
+    size: JsUint;
+  };
+}
+export namespace Network {
+  export type ResponseData = {
+    url: string;
+    protocol: string;
+    status: JsUint;
+    statusText: string;
+    fromCache: boolean;
+    headers: [...Network.Header[]];
+    mimeType: string;
+    bytesReceived: JsUint;
+    headersSize: JsUint | null;
+    bodySize: JsUint | null;
+    content: Network.ResponseContent;
+  };
+}
+export namespace Network {
+  export type BeforeRequestSent = {
+    method: "network.beforeRequestSent";
+    params: Network.BeforeRequestSentParameters;
+  };
+}
+export namespace Network {
+  export type BeforeRequestSentParameters = Network.BaseParameters & {
+    initiator: Network.Initiator;
+  };
+}
+export namespace Network {
+  export type FetchError = {
+    method: "network.fetchError";
+    params: Network.FetchErrorParameters;
+  };
+}
+export namespace Network {
+  export type FetchErrorParameters = Network.BaseParameters & {
+    errorText: string;
+  };
+}
+export namespace Network {
+  export type ResponseCompleted = {
+    method: "network.responseCompleted";
+    params: Network.ResponseCompletedParameters;
+  };
+}
+export namespace Network {
+  export type ResponseCompletedParameters = Network.BaseParameters & {
+    response: Network.ResponseData;
+  };
+}
+export namespace Network {
+  export type ResponseStarted = {
+    method: "network.responseStarted";
+    params: Network.ResponseStartedParameters;
+  };
+}
+export namespace Network {
+  export type ResponseStartedParameters = Network.BaseParameters & {
+    response: Network.ResponseData;
+  };
 }
 export type ScriptCommand =
   | Script.AddPreloadScriptCommand
@@ -320,6 +696,11 @@ export type ScriptCommand =
   | Script.Evaluate
   | Script.GetRealms
   | Script.RemovePreloadScriptCommand;
+export type ScriptResult =
+  | Script.AddPreloadScriptResult
+  | Script.EvaluateResult
+  | Script.GetRealmsResult;
+export type ScriptEvent = Script.RealmCreated | Script.RealmDestroyed;
 export namespace Script {
   export type Channel = string;
 }
@@ -477,6 +858,65 @@ export namespace Script {
   export type BigIntValue = {
     type: "bigint";
     value: string;
+  };
+}
+export namespace Script {
+  export type RealmInfo =
+    | Script.WindowRealmInfo
+    | Script.DedicatedWorkerRealmInfo
+    | Script.SharedWorkerRealmInfo
+    | Script.ServiceWorkerRealmInfo
+    | Script.WorkerRealmInfo
+    | Script.PaintWorkletRealmInfo
+    | Script.AudioWorkletRealmInfo
+    | Script.WorkletRealmInfo;
+}
+export namespace Script {
+  export type BaseRealmInfo = {
+    realm: Script.Realm;
+    origin: string;
+  };
+}
+export namespace Script {
+  export type WindowRealmInfo = Script.BaseRealmInfo & {
+    type: "window";
+    context: BrowsingContext.BrowsingContext;
+    sandbox?: string;
+  };
+}
+export namespace Script {
+  export type DedicatedWorkerRealmInfo = Script.BaseRealmInfo & {
+    type: "dedicated-worker";
+  };
+}
+export namespace Script {
+  export type SharedWorkerRealmInfo = Script.BaseRealmInfo & {
+    type: "shared-worker";
+  };
+}
+export namespace Script {
+  export type ServiceWorkerRealmInfo = Script.BaseRealmInfo & {
+    type: "service-worker";
+  };
+}
+export namespace Script {
+  export type WorkerRealmInfo = Script.BaseRealmInfo & {
+    type: "worker";
+  };
+}
+export namespace Script {
+  export type PaintWorkletRealmInfo = Script.BaseRealmInfo & {
+    type: "paint-worklet";
+  };
+}
+export namespace Script {
+  export type AudioWorkletRealmInfo = Script.BaseRealmInfo & {
+    type: "audio-worklet";
+  };
+}
+export namespace Script {
+  export type WorkletRealmInfo = Script.BaseRealmInfo & {
+    type: "worklet";
   };
 }
 export namespace Script {
@@ -707,8 +1147,14 @@ export namespace Script {
 export namespace Script {
   export type WindowProxyRemoteValue = {
     type: "window";
+    value: Script.WindowProxyProperties;
     handle?: Script.Handle;
     internalId?: Script.InternalId;
+  };
+}
+export namespace Script {
+  export type WindowProxyProperties = {
+    context: BrowsingContext.BrowsingContext;
   };
 }
 export namespace Script {
@@ -750,6 +1196,12 @@ export namespace Script {
   };
 }
 export namespace Script {
+  export type Source = {
+    realm: Script.Realm;
+    context?: BrowsingContext.BrowsingContext;
+  };
+}
+export namespace Script {
   export type RealmTarget = {
     realm: Script.Realm;
   };
@@ -774,6 +1226,11 @@ export namespace Script {
     functionDeclaration: string;
     arguments?: [...Script.ChannelValue[]];
     sandbox?: string;
+  };
+}
+export namespace Script {
+  export type AddPreloadScriptResult = {
+    script: Script.PreloadScript;
   };
 }
 export namespace Script {
@@ -839,6 +1296,11 @@ export namespace Script {
   };
 }
 export namespace Script {
+  export type GetRealmsResult = {
+    realms: [...Script.RealmInfo[]];
+  };
+}
+export namespace Script {
   export type RemovePreloadScriptCommand = {
     method: "script.removePreloadScript";
     params: Script.RemovePreloadScriptParameters;
@@ -847,6 +1309,83 @@ export namespace Script {
 export namespace Script {
   export type RemovePreloadScriptParameters = {
     script: Script.PreloadScript;
+  };
+}
+export namespace Script {
+  export type Message = {
+    method: "script.message";
+    params: Script.MessageParameters;
+  };
+}
+export namespace Script {
+  export type MessageParameters = {
+    channel: Script.Channel;
+    data: Script.RemoteValue;
+    source: Script.Source;
+  };
+}
+export namespace Script {
+  export type RealmCreated = {
+    method: "script.realmCreated";
+    params: Script.RealmInfo;
+  };
+}
+export namespace Script {
+  export type RealmDestroyed = {
+    method: "script.realmDestoyed";
+    params: Script.RealmDestroyedParameters;
+  };
+}
+export namespace Script {
+  export type RealmDestroyedParameters = {
+    realm: Script.Realm;
+  };
+}
+export type LogEvent = Log.EntryAdded;
+export namespace Log {
+  export const enum Level {
+    Debug = "debug",
+    Info = "info",
+    Warn = "warn",
+    Error = "error",
+  }
+}
+export namespace Log {
+  export type Entry =
+    | Log.GenericLogEntry
+    | Log.ConsoleLogEntry
+    | Log.JavascriptLogEntry;
+}
+export namespace Log {
+  export type BaseLogEntry = {
+    level: Log.Level;
+    source: Script.Source;
+    text: string | null;
+    timestamp: JsUint;
+    stackTrace?: Script.StackTrace;
+  };
+}
+export namespace Log {
+  export type GenericLogEntry = Log.BaseLogEntry & {
+    type: string;
+  };
+}
+export namespace Log {
+  export type ConsoleLogEntry = Log.BaseLogEntry & {
+    type: "console";
+    method: string;
+    args: [...Script.RemoteValue[]];
+  };
+}
+export namespace Log {
+  export type JavascriptLogEntry = Log.BaseLogEntry & {
+    type: "javascript";
+  };
+}
+export namespace Log {
+  export type EntryAdded = {
+    method: "log.entryAdded";
+    params: Log.Entry;
   };
 }
 export type InputCommand = Input.PerformActions | Input.ReleaseActions;
